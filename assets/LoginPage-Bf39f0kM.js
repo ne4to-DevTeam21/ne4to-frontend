7d70886b47eb8e7fb39a5ad969f7805609529036
TuyaraGrigoreva <tugrigoreva@samokat.ru>
2024-06-03 21:28:55 +0300

    Updates

---
+++ assets/LoginPage-Bf39f0kM.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BhIIHzBD.js";const n=()=>e.jsx("section",{children:e.jsx("div",{children:"LoginPage"})});export{n as LoginPage};
---
+++ assets/RulesPage-C66tMa6U.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BhIIHzBD.js";const r=()=>e.jsx("section",{children:e.jsx("div",{children:"RulesPage"})});export{r as RulesPage};
